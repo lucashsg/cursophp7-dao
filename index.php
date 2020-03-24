29d62dbb5b768f4ce5a1a4b1eff76a7d0fdb405a
Lucas Suzuki <lucashsg@outlook.com>
2020-03-23 22:35:13 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 
 require_once "config.php";
 
-$root = new Usuario();
+// Carrega um usuário
+// $root = new Usuario();
+// $root->loadById(3);
 
-$root->loadById(3);
+// Carrega uma lista de usuarios
+//$lista = Usuario::getList();
 
-echo $root;
+// Carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search("Lucas");
+// echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("llucas", "12345");
+
+echo $usuario;
 
 ?>
